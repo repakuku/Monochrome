33120cc6955c5d5f223b66b5b6324ebdbb6e3213
Alexey Turulin <repakuku@icloud.com>
2023-10-06 17:32:36 -0400

    working with ui

--- Monochrome/ContentView.swift
+++ Monochrome/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            Color(.gray)
+            Color(uiColor: .darkGray)
                 .ignoresSafeArea()
             
             VStack(spacing: 50) {
