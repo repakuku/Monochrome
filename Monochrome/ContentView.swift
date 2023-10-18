096dc22aaf1bac6380ace12e1d1fa48a5c6680dd
Alexey Turulin <repakuku@icloud.com>
2023-10-18 14:07:21 -0400

    working with game logic

--- Monochrome/ContentView.swift
+++ Monochrome/ContentView.swift
@@ -8,94 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var size = 2
-    @State private var field: [[Int]] = []
+	@State private var size = 2
+	@State private var field: [[Int]] = []
 
-    @State private var gameStarted = false
+	@State private var gameStarted = false
+	@State private var alertPresented = false
 
-    private let minOpacity = 0.4
-    private let maxOpacity = 1.0
+	private let minOpacity = 0.4
+	private let maxOpacity = 1.0
 
-    private let mainColor = "Main"
-    private let minorColor = "Minor"
-    private let backgroundColor = "Background"
+	private let majorColor = "Major"
+	private let minorColor = "Minor"
+	private let backgroundColor = "Background"
 
-    var body: some View {
-        ZStack {
-            Color(backgroundColor)
-                .opacity(0.6)
-                .ignoresSafeArea()
+	var body: some View {
+		ZStack {
+			Color(backgroundColor)
+				.opacity(0.6)
+				.ignoresSafeArea()
 
-            VStack(spacing: 30) {
-                Text("Monochrome")
-                    .foregroundStyle(Color(mainColor))
-                    .font(.title)
+			VStack(spacing: 30) {
+				Text("Monochrome")
+					.foregroundStyle(Color(majorColor))
+					.font(.title)
 
-                Spacer()
+				Spacer()
 
-                FieldView(field: $field, firstColor: mainColor, secondColor: minorColor)
+				FieldView(
+					field: $field,
+					alertPresented: $alertPresented,
+					majorColor: majorColor,
+					minorColor: minorColor
+				)
 
-                Spacer()
+				Spacer()
 
-                HStack(alignment: .center) {
-                    Button("-") {
-                        size -= size == 2 ? 0 : 2
-                    }
-                    .disabled(size == 2)
-                    .opacity(size == 2 ? minOpacity : maxOpacity)
-                    .font(.system(size: 100))
-                    .foregroundStyle(size == 2 ? Color(minorColor) : Color(mainColor))
+				HStack(alignment: .center) {
+					Button("-") {
+						size -= size == 2 ? 0 : 2
+					}
+					.disabled(size == 2)
+					.opacity(size == 2 ? minOpacity : maxOpacity)
+					.font(.system(size: 100))
+					.foregroundStyle(size == 2 ? Color(minorColor) : Color(majorColor))
 
-                    Text("\(size)x\(size)")
-                        .font(.system(size: 80))
-                        .frame(width: 240)
-                        .foregroundStyle(Color(mainColor))
+					Text("\(size)x\(size)")
+						.font(.system(size: 80))
+						.frame(width: 240)
+						.foregroundStyle(Color(majorColor))
 
-                    Button("+") {
-                        size += size == 10 ? 0 : 2
-                    }
-                    .disabled(size == 10)
-                    .opacity(size == 10 ? minOpacity : maxOpacity)
-                    .font(.system(size: 100))
-                    .foregroundStyle(size == 10 ? Color(minorColor) : Color(mainColor))
-                }
-                .animation(.default, value: size)
+					Button("+") {
+						size += size == 10 ? 0 : 2
+					}
+					.disabled(size == 10)
+					.opacity(size == 10 ? minOpacity : maxOpacity)
+					.font(.system(size: 100))
+					.foregroundStyle(size == 10 ? Color(minorColor) : Color(majorColor))
+				}
+				.animation(.default, value: size)
 
-                Button("Start New Game") {
-                    withAnimation {
-                        startNewGame(withFieldSize: size)
-                    }
-                }
-                .frame(width: 300, height: 60)
-                .foregroundStyle(Color(mainColor))
-                .font(.largeTitle)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color(mainColor), lineWidth: 3)
-                )
-            }
-            .shadow(radius: 5, x: 0.0, y: 0.0)
-        }
-        .onAppear {
-            withAnimation(Animation.easeInOut(duration: 1)) {
-                startNewGame(withFieldSize: size)
-            }
-        }
-    }
+				Button("Start New Game") {
+					withAnimation {
+						startNewGame(withFieldSize: size)
+					}
+				}
+				.frame(width: 300, height: 60)
+				.foregroundStyle(Color(majorColor))
+				.font(.largeTitle)
+				.overlay(
+					RoundedRectangle(cornerRadius: 10)
+						.stroke(Color(majorColor), lineWidth: 3)
+				)
+			}
+			.shadow(radius: 5, x: 0.0, y: 0.0)
+		}
+		.onAppear {
+			withAnimation(Animation.easeInOut(duration: 1)) {
+				startNewGame(withFieldSize: size)
+			}
+		}
+		.alert("Complete!", isPresented: $alertPresented) {
+			Button("Start new game") {
+				withAnimation {
+					startNewGame(withFieldSize: size)
+				}
+			}
+		}
+	}
 
-    private func startNewGame(withFieldSize fieldSize: Int) {
-        field = []
+	private func startNewGame(withFieldSize fieldSize: Int) {
+		field = []
 
-        for row in 0..<fieldSize {
-            field.append([])
-            for _ in 0..<fieldSize {
-                let cell = Int.random(in: 0...1)
-                field[row].append(cell)
-            }
-        }
-    }
+		for row in 0..<fieldSize {
+			field.append([])
+			for _ in 0..<fieldSize {
+				let cell = Int.random(in: 0...1)
+				field[row].append(cell)
+			}
+		}
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
--- Monochrome/FieldView.swift
+++ Monochrome/FieldView.swift
@@ -8,40 +8,55 @@
 import SwiftUI
 
 struct FieldView: View {
-    @Binding var field: [[Int]]
+	@Binding var field: [[Int]]
+	@Binding var alertPresented: Bool
 
-    let firstColor: String
-    let secondColor: String
+	let majorColor: String
+	let minorColor: String
 
-    @State private var alertPresented = false
+	var body: some View {
+		VStack(spacing: 2) {
+			ForEach(0..<field.count, id: \.self) { x in
+				HStack(spacing: 2) {
+					ForEach(0..<field.count, id: \.self) { y in
+						Color(field[x][y] == 0 ? minorColor : majorColor)
+							.clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
+							.onTapGesture {
+								changeColor(x: x, y: y)
+								checkGame()
+							}
+					}
+				}
+			}
+		}
+		.frame(width: 350, height: 350)
+	}
 
-    var body: some View {
-        VStack(spacing: 2) {
-            ForEach(0..<field.count, id: \.self) { x in
-                HStack(spacing: 2) {
-                    ForEach(0..<field.count, id: \.self) { y in
-                        Color(field[x][y] == 0 ? secondColor : firstColor)
-                            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
-                            .onTapGesture {
-                                changeColor(x: x, y: y)
-                            }
-                    }
-                }
-            }
-        }
-        .frame(width: 350, height: 350)
-        .alert("Complete!", isPresented: $alertPresented, actions: {})
-    }
+	private func changeColor(x: Int, y: Int) {
+		for index in 0..<field.count {
+			field[x][index] = 1 - field[x][index]
+			field[index][y] = 1 - field[index][y]
+		}
+		field[x][y] = 1 - field[x][y]
+	}
 
-    private func changeColor(x: Int, y: Int) {
-        for index in 0..<field.count {
-            field[x][index] = 1 - field[x][index]
-            field[index][y] = 1 - field[index][y]
-        }
-        field[x][y] = 1 - field[x][y]
-    }
+	private func checkGame() {
+		for row in field {
+			if row.contains(0) {
+				return
+			}
+		}
+
+		alertPresented.toggle()
+		return
+	}
 }
 
 #Preview {
-    FieldView(field: .constant([[0, 1], [1, 0]]), firstColor: "Main", secondColor: "Minor")
+	FieldView(
+		field: .constant([[0, 1], [1, 0]]),
+		alertPresented: .constant(false),
+		majorColor: "Major",
+		minorColor: "Minor"
+	)
 }
